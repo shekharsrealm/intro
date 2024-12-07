4499a02dc3f93472e9c86b5975a40379b1e6bc0b
Shekhar <sbhattacharya099@gmail.com>
2024-12-07 14:28:37 +0100

    Update design

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const slider = document.querySelector('.slider');
+const leftButton = document.querySelector('.slider-btn.left');
+const rightButton = document.querySelector('.slider-btn.right');
+
+leftButton.addEventListener('click', () => {
+  slider.scrollBy({
+    left: -200, // Adjust scroll distance
+    behavior: 'smooth'
+  });
+});
+
+rightButton.addEventListener('click', () => {
+  slider.scrollBy({
+    left: 200, // Adjust scroll distance
+    behavior: 'smooth'
+  });
+});
+
